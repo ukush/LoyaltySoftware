6f752bb08f6f360fdc88c513842a43ed57ad276d
Kemas Raihan <74256544+KemasRaihan@users.noreply.github.com>
2021-03-07 23:34:19 +0000

    Added DbConnection string class

---
+++ LoyaltySoftware/Pages/Shared/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoyaltySoftware.Pages.Shared
+{
+    public class DBConnection
+    {
+        public string DatabaseString()
+        {
+            string DbString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LoyaltySystem;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            return DbString;
+        }
+    }
+}
