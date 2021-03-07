9feed457b7f824e7449000749deef64547fe62da
Kemas Raihan <raihan.madani@gmail.com>
2021-03-07 22:40:45 +0000

    Added Admin Login Pages

--- LoyaltySoftware/Pages/Login/AdminLogin.cshtml.cs
+++ LoyaltySoftware/Pages/Login/AdminLogin.cshtml.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace LoyaltySoftware.Pages.Login
 {
-    public class loginModel : PageModel
+    public class AdminLoginModel : PageModel
     {
         public void OnGet()
         {
