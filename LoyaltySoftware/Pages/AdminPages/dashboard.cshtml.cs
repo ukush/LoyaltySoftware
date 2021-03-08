7c102b4942dd778870d2dbbe0231b8a269ffd7a2
Kemas Raihan <74256544+KemasRaihan@users.noreply.github.com>
2021-03-08 19:40:13 +0000

    Added cshtml.cs program for admin dashboard

--- LoyaltySoftware/Pages/AdminPages/dashboard.cshtml.cs
+++ LoyaltySoftware/Pages/AdminPages/dashboard.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,23 @@ namespace LoyaltySoftware.Pages.AdminPages
 {
     public class dashboardModel : PageModel
     {
-        public void OnGet()
+        public string Username;
+        public const string SessionKeyName1 = "username";
+
+        public string SessionID;
+        public const string SessionKeyName3 = "sessionID";
+
+        public IActionResult OnGet()
         {
+            Username = HttpContext.Session.GetString(SessionKeyName1);
+            SessionID = HttpContext.Session.GetString(SessionKeyName3);
+
+            if (string.IsNullOrEmpty(Username) && string.IsNullOrEmpty(SessionID))
+            {
+                return RedirectToPage("/Login/Login");
+            }
+            return Page();
+
         }
     }
 }
